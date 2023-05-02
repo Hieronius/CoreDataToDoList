9ce064b9667105bc9d3933a73c892a96afbf8698
Arsenty Halimovsky <ghost12@yandex.ru>
2023-05-02 22:36:10 +0300

    Add NaviVC

--- CoreDataToDoList/CoreDataToDoList/TableViewController.swift
+++ CoreDataToDoList/CoreDataToDoList/TableViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 final class TableViewController: UITableViewController {
     
+    // MARK: - Private Properties
+    
+    private var tasks = [String]()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // some code here
     }
+    
+    // MARK: - IBActions
+    
+    
+    
+    
+    // MARK: - TableView methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
 }
+
+
